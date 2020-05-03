f2ba9891248b2a586c522317edcca2dbe0e09e79
Francisco Campos <franciiscocampos170@gmail.com>
2020-05-03 16:17:16 +0100

    “files”

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,10 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
-            $url->forceSchema('https');
-        }
+        //
     }
 }
